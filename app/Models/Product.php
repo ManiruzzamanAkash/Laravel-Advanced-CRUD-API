d225ca467dd15ddc1d1b1b1c54e663584a24fd59
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-14 01:20:36 +0600

    Product Model Fillable Property Added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'image'
+    ];
 }
