6a8fd1f21f143463430e0bf372dc9388efd856f4
MD. Maniruzzaman Akash <manirujjamanakash@gmail.com>
2020-12-15 07:32:42 +0600

    User table seeder done

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->truncate();
+        $data = [
+            'name' => 'Maniruzzaman Akash',
+            'email' => 'manirujjamanakash@gmail.com',
+            'password' => Hash::make('123456')
+        ];
+        User::create($data);
+    }
+}
