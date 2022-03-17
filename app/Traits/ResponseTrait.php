129885392e8bbd6f43ae2d0bb2c470c826f07a50
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-03-17 12:23:01 +0600

    Removed response repository and added Reposne trait

---
+++ app/Traits/ResponseTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ResponseTrait
+{
+    /**
+     * Generate success type response.
+     *
+     * Returns the success data and message if there is any error
+     *
+     * @param object $data
+     * @param string $message
+     * @param integer $status_code
+     * @return JsonResponse
+     */
+    public function responseSuccess($data, $message = "Successful", $status_code = JsonResponse::HTTP_OK): JsonResponse
+    {
+        return response()->json([
+            'status'  => true,
+            'message' => $message,
+            'errors'  => null,
+            'data'    => $data,
+        ], $status_code);
+    }
+
+    /**
+     * Generate Error response.
+     *
+     * Returns the errors data if there is any error
+     *
+     * @param object $errors
+     * @return JsonResponse
+     */
+    public function responseError($errors, $message = 'Data is invalid', $status_code = JsonResponse::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json([
+            'status'  => false,
+            'message' => $message,
+            'errors'  => $errors,
+            'data'    => null,
+        ], $status_code);
+    }
+}
